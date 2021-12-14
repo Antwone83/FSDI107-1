aacdcd4db627f905609781200f83d9008884b0e4
Antwone83 <92768235+Antwone83@users.noreply.github.com>
2021-12-13 19:58:41 -0800

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h4>Eve n Adam Adult Shop 2002. All rights reserved</h4>
+            <h5>Antwone Adams</h5>
+            </div>
+
+
+    );
+}
+
+export default Footer;
